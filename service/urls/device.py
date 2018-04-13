9fb5fa3a57e961b417eee54e92b715fa41e150ce
Y4n <siyu@blocks.tech>
2018-04-13 19:46:56 +0800

    feat
    add api url

--- service/urls/device.py
+++ service/urls/device.py
@@ -1,7 +1,13 @@
 from django.conf.urls import url
 from service.views.device_api import (
-    AliasAPIView, )
+    AliasAPIView, CtrlTagAPIView, DeviceAPIView, TagAPIView, TagCheckAPIView, UserTagAPIView, DeviceMobileUpdateAPIView)
 
 urlpatterns = [
     url(r'^alias/?$', AliasAPIView.as_view(), name='alias'),
+    url(r'^device/?$', DeviceAPIView.as_view(), name='alias'),
+    url(r'^device/mobile?$', DeviceMobileUpdateAPIView.as_view(), name='alias'),
+    url(r'^tag/?$', TagAPIView.as_view(), name='alias'),
+    url(r'^tag/ctrl?$', CtrlTagAPIView.as_view(), name='alias'),
+    url(r'^tag/check?$', TagCheckAPIView.as_view(), name='alias'),
+    url(r'^tag/user?$', UserTagAPIView.as_view(), name='alias'),
 ]
