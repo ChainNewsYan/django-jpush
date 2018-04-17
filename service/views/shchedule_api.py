551d2c6f2a9b83b5c4ddaa7f68e0f364a5edc587
Y4n <siyu@blocks.tech>
2018-04-17 12:41:26 +0800

    update api

--- service/views/shchedule_api.py
+++ service/views/shchedule_api.py
@@ -30,22 +30,30 @@ class ScheduleListAPIView(APIView, DJPushSchedule):
 
 class ScheduleAPIView(APIView, DJPushSchedule):
     def post(self):
+        _data = self.request.DATA
+        alert = _data.get('alert')
+        time = _data.get('time')  # "2016-07-17 12:00:00"
+        name = _data.get('name')
         push = self.my_push
         push.audience = jpush.all_
-        push.notification = jpush.notification(alert="Hello, world!")
+        push.notification = jpush.notification(alert=alert)
         push.platform = jpush.all_
         push = push.payload
-
-        trigger = jpush.schedulepayload.trigger("2016-07-17 12:00:00")
-        schedulepayload = jpush.schedulepayload.schedulepayload("name", True, trigger, push)
+        trigger = jpush.schedulepayload.trigger(time)
+        schedulepayload = jpush.schedulepayload.schedulepayload(name, True, trigger, push)
         return self.my_schedule.post_schedule(schedulepayload)
 
     def put(self):
+        _data = self.request.DATA
+        alert = _data.get('alert')  # "Hello, world!"
+        time = _data.get('time')  # "2016-07-17 12:00:00"
+        name = _data.get('name')
+        schedule_id = _data.get('schedule_id')  # "17349f00-0852-11e6-91b1-0021f653c902"
         push = self.my_push
         push.audience = jpush.all_
-        push.notification = jpush.notification(alert="Hello, world!")
+        push.notification = jpush.notification(alert=alert)
         push.platform = jpush.all_
         push = push.payload
-        trigger = jpush.schedulepayload.trigger("2016-05-17 12:00:00")
-        schedulepayload = jpush.schedulepayload.schedulepayload("update a new name", True, trigger, push)
-        return self.my_schedule.put_schedule(schedulepayload, "17349f00-0852-11e6-91b1-0021f653c902")
+        trigger = jpush.schedulepayload.trigger(time)
+        schedulepayload = jpush.schedulepayload.schedulepayload(name, True, trigger, push)
+        return self.my_schedule.put_schedule(schedulepayload, schedule_id)
---
+++ service/views/zone_api.py
@@ -0,0 +1,49 @@
+import jpush
+from jpush import common
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from djpush.zone import DJPushZone
+
+
+class ZoneDefaultAPIView(APIView, DJPushZone):
+    def post(self):
+        _data = self.request.DATA
+        alert = _data.get('alert')
+        push = self.my_push
+        push.audience = jpush.all_
+        push.notification = jpush.notification(alert=alert)
+        push.platform = jpush.all_
+        try:
+            response = push.send()
+        except common.Unauthorized:
+            raise common.Unauthorized("Unauthorized")
+        except common.APIConnectionException:
+            raise common.APIConnectionException("conn")
+        except common.JPushFailure:
+            print("JPushFailure")
+        except:
+            print("Exception")
+        return Response(response)
+
+
+class ZoneBJAPIView(APIView, DJPushZone):
+
+    def post(self):
+        _data = self.request.DATA
+        alert = _data.get('alert')
+        push = self.my_push
+        push.audience = jpush.all_
+        push.notification = jpush.notification(alert=alert)
+        push.platform = jpush.all_
+        try:
+            response = push.send()
+        except common.Unauthorized:
+            raise common.Unauthorized("Unauthorized")
+        except common.APIConnectionException:
+            raise common.APIConnectionException("conn")
+        except common.JPushFailure:
+            print("JPushFailure")
+        except:
+            print("Exception")
+        return Response(response)
